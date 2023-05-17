aab6d24ec323c1dcd244e89e03d5a660715505dc
Nemo-Looking-For <nnemshilov11@gmail.com>
2023-05-17 14:21:48 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import"./App.css";
 import NavBar from "./components/navbar/NavBar";
+import Banner from "./components/banner/Banner"
+import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
